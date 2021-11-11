6e48d742308de8246a481700d61082ad59ea8ae3
iiiivaska <iiiivaska@yandex.ru>
2021-11-11 19:28:58 +0300

    One-to-many relationships with Core Data, SwiftUI, and @FetchRequest

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -11,39 +11,47 @@ import CoreData
 struct ContentView: View {
     
     @Environment(\.managedObjectContext) var moc
-    @State private var lastNameFilter = "A"
+    @FetchRequest(entity: Country.entity(), sortDescriptors: []) var countries: FetchedResults<Country>
     
     var body: some View {
         VStack {
-            
-            FilteredList(filterKey: "lastName", filterValue: lastNameFilter) { (singer: Singer) in
-                Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-            }
-            
-            Button("Add Examples") {
-                let taylor = Singer(context: self.moc)
-                taylor.firstName = "Taylor"
-                taylor.lastName = "Swift"
-
-                let ed = Singer(context: self.moc)
-                ed.firstName = "Ed"
-                ed.lastName = "Sheeran"
-
-                let adele = Singer(context: self.moc)
-                adele.firstName = "Adele"
-                adele.lastName = "Adkins"
-                    
-                if (moc.hasChanges) {
-                    try? self.moc.save()
+            List {
+                ForEach(countries, id: \.self) { country in
+                    Section(country.wrappedFullName) {
+                        ForEach(country.candyArray, id: \.self) { candy in
+                            Text(candy.wrappedName)
+                        }
+                    }
                 }
             }
+            
+            Button("Add") {
+                moc.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+                let candy1 = Candy(context: self.moc)
+                candy1.name = "Mars"
+                candy1.origin = Country(context: self.moc)
+                candy1.origin?.shortName = "UK"
+                candy1.origin?.fullName = "United Kingdom"
 
-            Button("Show A") {
-                self.lastNameFilter = "A"
-            }
+                let candy2 = Candy(context: self.moc)
+                candy2.name = "KitKat"
+                candy2.origin = Country(context: self.moc)
+                candy2.origin?.shortName = "UK"
+                candy2.origin?.fullName = "United Kingdom"
 
-            Button("Show S") {
-                self.lastNameFilter = "S"
+                let candy3 = Candy(context: self.moc)
+                candy3.name = "Twix"
+                candy3.origin = Country(context: self.moc)
+                candy3.origin?.shortName = "UK"
+                candy3.origin?.fullName = "United Kingdom"
+
+                let candy4 = Candy(context: self.moc)
+                candy4.name = "Toblerone"
+                candy4.origin = Country(context: self.moc)
+                candy4.origin?.shortName = "CH"
+                candy4.origin?.fullName = "Switzerland"
+
+                try? self.moc.save()
             }
         }
     }
